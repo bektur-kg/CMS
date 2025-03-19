b1ffad506b3b6212b41cca1e7fe254aeffe2f98e
bektur-kg <roadtothedreambeka81@gmail.com>
2025-03-19 18:25:15 +0600

    added global exception handling

--- CMS.Api/Program.cs
+++ CMS.Api/Program.cs
@@ -1,4 +1,5 @@
 using CMS.Api.Extensions;
+using CMS.Api.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseAuthentication();
 app.UseAuthorization();
